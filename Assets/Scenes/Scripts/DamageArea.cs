9f18976971ca2dbe18b4025783061e765bd67c0f
Chan <Donny.Tran@elev.agderskolen.no>
2018-01-03 15:08:20 +0100

    Added attack system for enemies, moved chasing system to enemy.

--- Assets/Scenes/Scripts/DamageArea.cs
+++ Assets/Scenes/Scripts/DamageArea.cs
@@ -4,28 +4,66 @@ using UnityEngine;
 
 public class DamageArea : MonoBehaviour {
 
-	protected float delay;
+	protected float attackCooldown = 3f;
+	private float enterAreaTime = 0f;
+
 	public float damage = 30;
 
 	public GameObject target;
 
+	private Enemy enemy;
+
 	//cone
-	private Vector3 coneDir = new Vector3(0, 0, 1);
-	private float coneCenter = transform.position;
-	private float coneRadius = 0.7f;
-	private float coneAngle = 1f;
+	private Vector3 coneCenter;
+	private float coneRadius = 1.5f;
+	private float coneAngle = 120f; //in degrees
+
+	//temp
+	private Vector3 leftVector;
+	private Vector3 rightVector;
 
 	void Start () {
-		
+		enemy = GetComponent<Enemy> ();
 	}
 
 	void Update () {
+		leftVector = Quaternion.AngleAxis (-coneAngle / 2, Vector3.up) * transform.forward;
+		rightVector = Quaternion.AngleAxis (coneAngle / 2, Vector3.up) * transform.forward;
+
+		Debug.DrawRay (transform.position, leftVector * 10, Color.red);
+		Debug.DrawRay (transform.position, rightVector * 10, Color.red);
+
 		if (isInsideCone(target)){
-			
+			if (!enemy.isAttacking) { //if just entered the cone
+				enterAreaTime = Time.time;
+				StartAttack ();
+			} else { //if stayed in the cone
+				if (Time.time - enterAreaTime >= attackCooldown) {
+					print ("You are damaged");
+					enemy.isAttacking = false;
+				}
+			}
+		} else {
+			enterAreaTime = 0;
+			enemy.isAttacking = false;
 		}
 	}
 
 	private bool isInsideCone(GameObject target){
-		
+		coneCenter = transform.position;
+		Vector3 targetPos = target.transform.position;
+		if ((targetPos - coneCenter).magnitude <= coneRadius) { //check if within radius
+			Vector3 vecToTarget = targetPos - coneCenter;
+			if(Vector3.Angle(transform.forward, vecToTarget) <= coneAngle / 2){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private void StartAttack(){
+		enemy.isAttacking = true;
+		//play attack animation
+		transform.LookAt(target.transform.position);
 	}
 }
--- Assets/Scenes/Scripts/EnemyMovement.cs
+++ Assets/Scenes/Scripts/EnemyMovement.cs
@@ -5,58 +5,21 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour {
 
-
     private NavMeshAgent pathFinder;
-    public Player targetPlayer;
-    private CapsuleCollider capsCol;
-    private float provocRange = 10;
+    
+	private bool isChasing = false;
 
-    private Enemy self;
-
-	// Use this for initialization
 	void Start () {
         pathFinder = GetComponent<NavMeshAgent>();
-        //targetPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-        //capsCol = GetComponent<CapsuleCollider>();
-
-        self = GetComponent<Enemy>();
 	}
 
-	// Update is called once per frame
-	void Update () {
-		if (!self.isStunned) {
-			CheckChase ();
-		}
-	}
-
-    void CheckChase()
-    {
-        Vector3 pos = transform.position;
-        Vector3 targetPos = targetPlayer.transform.position;
-
-		if ((pos - targetPos).magnitude <= transform.localScale.x/2 + targetPlayer.transform.localScale.x/2)
-        {
-            StopChase();
-        }
-        else if ((pos - targetPos).magnitude <= provocRange)
-        {
-            Chase(targetPos);
-        }
-        else
-        {
-            StopChase();
-        }
-    }
-
-    void StopChase()
-    {
+    public void StopChase(){
         pathFinder.isStopped = true;
         pathFinder.SetDestination(transform.position);
     }
 
-    void Chase(Vector3 target)
-    {
-        pathFinder.SetDestination(target);
+    public void StartChase(GameObject target){
+		pathFinder.SetDestination(target.transform.position);
         pathFinder.isStopped = false;
     }
 }
